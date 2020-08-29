85a5fec6c4b79e9c1569e8ac18fa54b0562376bd
Mystique <13oct08@quantumfoam.uni.cc>
2020-08-29 23:54:06 +0200

    LGTM_Moved_Url

--- load_generator_stacks/locust_configs/locustfile.py
+++ load_generator_stacks/locust_configs/locustfile.py
@@ -4,21 +4,20 @@ import sys
 
 from locust import HttpLocust, TaskSet, between, task
 
-
 class xrayAppTasks(TaskSet):
     @task(1)
     def index(self):
-        self.client.get("/")
+        HOT_TOPICS = ["trace", "storage", "lambda",
+                      "database", "compute", "network"]
+        url_suffix = f"/{random.choice(HOT_TOPICS)}"
+        self.client.get(url_suffix, name="polyglot_svc")
         print(f"Locust instance {os.path.basename(__file__)}")
-        pass
 
     @task(2)
     def polyglot_svc(self):
-        # Add containers
         # headers={'X-Requested-With': 'XMLHttpRequest'})
-        url_suffix = f"/"
-        # Statistics for these requests will be grouped under: hot_jobs
-        self.client.get(url_suffix, name="polyglot_svc")
+        url_suffix = f"/aws"
+        self.client.get(url_suffix, name="polyglot_svc_observability")
         print(f"Running 'polyglot_svc'")
 
 
