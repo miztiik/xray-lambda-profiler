c6482e42ce6ef0da5e6f2f0fa755ca2f08e5d351
Mystique <13oct08@quantumfoam.uni.cc>
2020-08-30 00:25:13 +0200

    Updated_response_json

--- load_generator_stacks/locust_configs/locustfile.py
+++ load_generator_stacks/locust_configs/locustfile.py
@@ -2,7 +2,7 @@ import random
 import os
 import sys
 
-from locust import HttpUser, TaskSet, between, task
+from locust import HttpUser, task, TaskSet, between
 
 
 class xrayAppTasks(TaskSet):
@@ -11,17 +11,20 @@ class xrayAppTasks(TaskSet):
         HOT_TOPICS = ["trace", "storage", "lambda",
                       "database", "compute", "network"]
         url_suffix = f"/{random.choice(HOT_TOPICS)}"
-        self.client.get(url_suffix, name="polyglot_svc")
+        response = self.client.get(url_suffix, name="polyglot_svc")
+        print(f'{{"resp_status_code":{response.status_code}}}')
         print(f"Locust instance {os.path.basename(__file__)}")
 
     @task(2)
     def polyglot_svc(self):
         # headers={'X-Requested-With': 'XMLHttpRequest'})
         url_suffix = f"/aws"
-        self.client.get(url_suffix, name="polyglot_svc_observability")
+        response = self.client.get(
+            url_suffix, name="polyglot_svc_observability")
+        print(f'{{"resp_status_code":{response.status_code}}}')
         print(f"Running 'polyglot_svc'")
 
 
 class WebsiteUser(HttpUser):
-    task_set = [xrayAppTasks]
+    tasks = [xrayAppTasks]
     wait_time = between(1, 2)
