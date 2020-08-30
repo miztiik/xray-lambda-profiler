90b87a7f0e0e1bb48e673b480e7862f56e4f26d6
Mystique <13oct08@quantumfoam.uni.cc>
2020-08-30 02:15:42 +0200

    l

--- load_generator_stacks/locust_configs/locustfile.py
+++ load_generator_stacks/locust_configs/locustfile.py
@@ -2,24 +2,33 @@ import random
 import os
 import sys
 
-from locust import HttpUser, task, between
+
+from locust import HttpUser, TaskSet, task, between
 
 
-class xrayAppTasks(HttpUser):
-    @task(2)
-    def topics_generic(self):
-        HOT_TOPICS = ["trace", "storage", "lambda",
-                      "database", "compute", "network"]
-        url_suffix = f"/{random.choice(HOT_TOPICS)}"
-        response = self.client.get(url_suffix, name="polyglot_svc_generic")
-        print(f'{{"resp_status_code":{response.status_code}}}')
-        print(f"Locust instance {os.path.basename(__file__)}")
+def topics_generic(l):
+    HOT_TOPICS = ["trace", "storage", "lambda",
+                  "database", "compute", "network"]
+    url_suffix = f"/{random.choice(HOT_TOPICS)}"
+    response = l.client.get(url_suffix, name="polyglot_svc")
+    print(f'{{"resp_status_code":{response.status_code}}}')
+    print(f"Locust instance {os.path.basename(__file__)}")
 
-    @task(1)
-    def topic_aws(self):
-        # headers={'X-Requested-With': 'XMLHttpRequest'})
-        url_suffix = f"/aws"
-        response = self.client.get(
-            url_suffix, name="polyglot_svc_observability_aws")
-        print(f'{{"resp_status_code":{response.status_code}}}')
-        print(f"Running 'polyglot_svc'")
+
+def topic_aws(l):
+    # headers={'X-Requested-With': 'XMLHttpRequest'})
+    url_suffix = f"/aws"
+    response = l.client.get(
+        url_suffix, name="polyglot_svc_observability")
+    print(f'{{"resp_status_code":{response.status_code}}}')
+    print(f"Running 'polyglot_svc'")
+
+
+class UserTasks(TaskSet):
+    # one can specify tasks like this
+    tasks = [topics_generic, topic_aws]
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 2)
+    tasks = [UserTasks]
